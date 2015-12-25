9318f3db5c60a36f4ebd2a9dbf6a6bd28fa0a8a2
Giovanni Sferro <agi.novanta@gmail.com>
2015-12-25 06:22:48 +0100

    Add full block test.

--- src/src/test/net.rs
+++ src/src/test/net.rs
@@ -2,6 +2,13 @@ use net::*;
 use net::messages::*;
 use utils::Debug;
 use std::io::Cursor;
+use std::fs::File;
+
+#[test]
+fn test_block() {
+    let mut block_data = File::open("src/test/block.dat").unwrap();
+    let block = BlockMessage::deserialize(&mut block_data).unwrap();
+}
 
 #[test]
 fn test_version_message() {
